539d569ec7dffd68ab45d02a722f048808e948d9
Andrei Bucin <abucin@gmail.com>
2020-03-29 15:51:00 +0300

    Switched chart vis lib to NIVO.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe2141dfb73970897d944e5de4c3a26b.js"
+  "/precache-manifest.4f47d8572002e63a1554354646c45a7f.js"
 );
 
 self.addEventListener('message', (event) => {
